faf694051c888b62f89fbf6b27dd47ce7bedf48e
BlakeBrewen <brewen.blake4@gmail.com>
2023-10-05 19:07:36 -0500

    first commit

---
+++ TwittercloneBB/Data/TwitterDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourApp.Models; // Replace with the appropriate namespace for your models
+
+namespace TwittercloneBB.Data
+{
+    public class TwitterDbContext
+    {
+    }
+}
---
+++ TwittercloneBB/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TwittercloneBB.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
