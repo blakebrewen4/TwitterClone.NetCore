22b0db94a6674204b15858af2cbc4b87ab5c38db
BlakeBrewen <brewen.blake4@gmail.com>
2023-10-06 22:33:38 -0500

    conecctions

---
+++ TwittercloneBB/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwittercloneBB.Data;
+
+public class UserController : Controller
+{
+    private readonly TwitterDbContext _context;
+
+    public UserController(TwitterDbContext context)
+    {
+        _context = context;
+    }
+
+    // Use _context to interact with the database
+}
+
